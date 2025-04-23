359f8aac531a1f3098826275e5e966fe47077781
FreezeYourDiary <freezeyourdiary@gmail.com>
2025-04-23 13:15:17 +0200

    strony dla panstwa, zmiany w home page
    Zmieniono funkcjonalność strony home na "wyszukiwarkę" państw
    Dane o państwie przeniesiono na stronę z danymi szczegółowymi o wybranym państwie.
    + możliwość pobrania wykresu danych ( dodać:    Możliwość podania nazwy wykresu.,wybór ścieżki formatu pliku )

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from pages import views
 
 urlpatterns = [
-    path("", views.home, name='home')
+    path("", views.home, name='home'),
+    path('country/<str:country_code>/', views.country_view, name='country_view') # np. link http://127.0.0.1:8000/country/PL/
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from .models import EnergyData, Country
-# Create your views here.
 
 
 def home(request):
-    selected_country_code = request.GET.get(
-        'country', 'PL')  # domyślnie Polska
     countries = Country.objects.all()
+    return render(request, "pages/home.html", {'countries': countries})
+
+# funkcjonalnosc home() przeniesiono w country_view().
+def country_view(request, country_code):
+    countries = Country.objects.all()
+    selected_country = Country.objects.get(code=country_code)
 
     data = EnergyData.objects.select_related(
         'source', 'country', 'category'
-    ).filter(country__code=selected_country_code, category__name='Production')
+    ).filter(country__code=country_code, category__name='Production')
 
-    # Zgrupuj dane: { źródło: {rok: wartość, ...}, ... }
     table = {}
     years = sorted(set(d.year for d in data))
     for record in data:
@@ -23,19 +25,31 @@ def home(request):
             table[source] = {}
         table[source][year] = value
 
-    # Oblicz sumy roczne
     from collections import defaultdict
     total_by_year = defaultdict(float)
     for record in data:
         total_by_year[record.year] += record.value
 
+    # param for graph
+    graph_type = request.GET.get('graph_type', 'bar')  # 'bar' -- default
+    year_range = request.GET.get('year_range', None)
+
+    if year_range:
+        year_range = list(map(int, year_range.split(',')))
+        data = [d for d in data if d.year in year_range]
+
     context = {
         'countries': countries,
-        'selected_country': selected_country_code,
+        'selected_country': selected_country.name, # full name for details.html "selected_country" par.
         'table': table,
         'years': years,
         'total_by_year': total_by_year,
         'total_years': [y for y in years if y in total_by_year],
         'total_values': [round(total_by_year[y], 3) for y in years if y in total_by_year],
+        'graph_type': graph_type,
+        'year_range': year_range,
     }
-    return render(request, "pages/home.html", context)
+    return render(request, "pages/details.html", context)
+
+# ?+ jesli wszystkie wiersze w tabeli sa puste nie wyswietlac caly wiersz
+
