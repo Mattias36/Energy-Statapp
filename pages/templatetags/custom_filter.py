d2b180b5618c326d903f38c8c724d54ec16d7e0f
Mati <mateuszmarciak36@gmail.com>
2025-05-28 14:09:27 +0200

    frontend dla categories i krajów

--- pages/templatetags/custom_filter.py
+++ pages/templatetags/custom_filter.py
@@ -1,4 +1,6 @@
 from django import template
+import re
+from django.utils.text import slugify
 
 register = template.Library()
 
@@ -6,3 +8,14 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.filter
+def remove_units(value):
+    """Usuwa jednostki w nawiasach kwadratowych, np. [GWh]"""
+    return re.sub(r'\s*\[.*?\]', '', value)
+
+@register.filter
+def to_slug(value):
+    clean = re.sub(r'\s*\[.*?\]', '', value)  # usuń jednostki
+    return slugify(clean)
